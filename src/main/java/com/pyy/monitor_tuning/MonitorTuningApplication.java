0d00f89c2fd113d27258cb2862d35f385cf12577
wolf-mac <panyangyang@163.com>
2018-09-21 15:31:40 +0800

    初始化项目

---
+++ src/main/java/com/pyy/monitor_tuning/MonitorTuningApplication.java
@@ -0,0 +1,12 @@
+package com.pyy.monitor_tuning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MonitorTuningApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MonitorTuningApplication.class, args);
+	}
+}
